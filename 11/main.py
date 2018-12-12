08a6b32273cfe01021c1b6a42d0bf16fafd190cd
Dawid Gwiżdż <dawcio.gwizdz@gmail.com>
2018-12-12 14:42:42 +0100

    Day 11 secon part

--- 11/main.py
+++ 11/main.py
@@ -25,3 +25,23 @@ for i in range(1,299):
 
 print maximum
 print x,y
+
+#part2
+from math import sqrt
+maximum = -5*300*300
+notfind = 0
+for s in range(0,int(sqrt(300))+1):
+	for i in range(1,301-s+1):
+		for j in range(1,301-s+1):
+			power=0
+			for i2 in range(i,i+s):
+				for j2 in range(j,j+s):
+					power += grid[i2][j2]
+			if power>maximum:
+				maximum=power
+				x = i
+				y = j
+				size = s
+
+print maximum
+print x,y,size
